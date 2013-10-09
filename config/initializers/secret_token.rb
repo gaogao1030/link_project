fd2fa3bcdd8365764daaf6b1daa107e86f19553b
GitCat <gitcat@gitcat.(none)>
2013-10-09 22:40:27 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkProject::Application.config.secret_token = '648aa6faf0110800d346f8bc1a7b25ff13154901d1fb5815c5f9d6d69ed02119635e10faf6299c1db009a84643ed639807d88114e76918ca944378d557809c19'
