12d8ab4024b1fc0ca640d279f6b5804742659404
GitCat <gitcat@gitcat.(none)>
2013-10-11 22:31:20 +0800

    add edit show delete

--- app/controllers/link_infos_controller.rb
+++ app/controllers/link_infos_controller.rb
@@ -10,4 +10,31 @@ class LinkInfosController < ApplicationController
 			redirect_to root_path
 		end
 	end
+
+	def index
+		@link_infos = LinkInfo.all
+	end
+
+	def show
+		@link_info = LinkInfo.find(params[:id])
+	end
+
+	def edit
+	@link_info = LinkInfo.find(params[:id])
+	end
+
+	def update
+    if @link_info.update_attributes(params[:link_info])
+      flash[:success] = "linkinfo updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    LinkInfo.find(params[:id]).destroy
+    flash[:success] = "linkinfo destroyed."
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ LinkProject::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
+  match 'sign_out' => 'users#sign_out'
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
@@ -52,7 +53,7 @@ LinkProject::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'homepage#index'
+  root :to => 'link_infos#index'
 
   # See how all your routes lay out with "rake routes"
 
